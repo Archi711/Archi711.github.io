669617cab5f206f73d86af0c3c5e3d92c3062d30
Archi711 <mlody711@o2.pl>
2018-10-23 11:15:26 +0200

    code

---
+++ precache-manifest.091115381f760a4a7db0627b5d093df1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7b366471f9c0f15c2849",
+    "url": "/static/js/main.7b366471.chunk.js"
+  },
+  {
+    "revision": "169acae9f9a2fabdbbb8",
+    "url": "/static/js/1.169acae9.chunk.js"
+  },
+  {
+    "revision": "7b366471f9c0f15c2849",
+    "url": "/static/css/main.b93153e8.chunk.css"
+  },
+  {
+    "revision": "2a0fc741068bd4a48ed73a1289093183",
+    "url": "/index.html"
+  }
+];
